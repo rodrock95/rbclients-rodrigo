8ffa8711745e2c097de30d9ee95f64ddbd11a52e
rodrock95 <r.barboza9502@gmail.com>
2022-03-22 16:49:48 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/rbclients/RbclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rbclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
